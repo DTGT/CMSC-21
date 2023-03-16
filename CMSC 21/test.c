74b88c9ef1e3da4e3cf072bc36d151e710a9b7e6
DTGT <65148026+DTGT@users.noreply.github.com>
2023-03-16 15:47:55 +0800

    Add files via upload

---
+++ CMSC 21/test.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+
+	// ######################## Check if number is positive or negative ##########################
+    // int num;
+
+    // printf("Enter a number: ");
+    // scanf("%d", &num);
+
+    // if (num >= 0) {
+    //     printf("%d is a positive number.\n", num);
+    // } else {
+    //     printf("%d is a negative number.\n", num);
+    // }
+	// ###########################################################################################
+
+
+	// ######################## Grade checker using switch case ##################################
+	// char grade;
+
+    // printf("Enter your grade (A, B, C, D, or F): ");
+    // scanf(" %c", &grade);
+
+    // switch (grade) {
+    //     case 'A':
+    //         printf("Excellent!\n");
+    //         break;
+    //     case 'B':
+    //         printf("Good job!\n");
+    //         break;
+    //     case 'C':
+    //         printf("You passed.\n");
+    //         break;
+    //     case 'D':
+    //         printf("You barely passed.\n");
+    //         break;
+    //     case 'F':
+    //         printf("You failed.\n");
+    //         break;
+    //     default:
+    //         printf("Invalid grade.\n");
+    //         break;
+    // }
+	// ###########################################################################################
+
+	// ######################## Conditional operators ############################################
+
+	// int num1, num2, max;
+
+    // printf("Enter two numbers: ");
+    // scanf("%d %d", &num1, &num2);
+
+    // max = (num1 > num2) ? num1 : num2;
+
+    // printf("The maximum of %d and %d is %d.\n", num1, num2, max);
+    // return 0;
+
+	// ###########################################################################################
+
+	// ######################## Nested if else #####	##############################################
+	// int marks;
+    // printf("Enter your marks (out of 100): ");
+    // scanf("%d", &marks);
+
+    // if(marks >= 60) {
+    //     printf("You have passed the exam. ");
+    //     if(marks >= 90){
+    //         printf("You have scored an A grade.\n");
+    //     }
+    //     else if(marks >= 80){
+    //         printf("You have scored a B grade.\n");
+    //     }
+    //     else if(marks >= 70){
+    //         printf("You have scored a C grade.\n");
+    //     }
+    //     else{
+    //         printf("You have scored a D grade.\n");
+    //     }
+    // }
+    // else {
+    //     printf("You have failed the exam. ");
+    //     if(marks >= 40){
+    //         printf("But you are eligible for a re-exam.\n");
+    //     }
+    //     else{
+    //         printf("And you are not eligible for a re-exam.\n");
+    //     }
+    // }
+	// ###########################################################################################
+
+
+
+	
+
+
+}
